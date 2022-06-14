0793ee4ae77d407ec216fa1f30ef6017f1cfe732
Cabyca <cabyca@yandex.ru>
2022-06-14 15:15:23 +0300

    heroku2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') !== 'local') {
+            \URL::forceScheme('https');
+        }
+
         Paginator::useBootstrapFive();
         //Paginator::useBootstrapFour();
         \Form::component('bsSelect', 'components.form.select', [
